6b9d197f2e636c9291a4491bdad360cec3f5e9c5
vedantsaxena <tanmay.saxena@gmail.com>
2020-05-29 09:22:53 +0530

    initial commit

---
+++ Me.js
@@ -0,0 +1,29 @@
+class Me extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/me.png");
+    this.smokeImage = loadImage("sprites/smoke.png");
+    this.trajectory =[];
+    
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+
+    super.display();
+
+    if(this.body.velocity.x > 15 && this.body.position.x > 300){
+      var position = [this.body.position.x, this.body.position.y];
+      this.trajectory.push(position);
+    }
+   
+
+    for(var i=0; i<this.trajectory.length; i++){
+      image(this.smokeImage, this.trajectory[i][0], this.trajectory[i][1]);
+    }
+    
+  }
+   
+  }
+
